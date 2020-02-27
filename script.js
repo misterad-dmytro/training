b40aa97137e97c4bcb0820445a7c6eac608a149f
Dmytro Misterad <anikin.dima3@gmail.com>
2020-02-27 06:48:06 -0500

    Второй урок - Циклы и доработка кода

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
-// let money = prompt("Ваш бюджет на месяц?", ''),
-// 	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-// let appData = {
-// 	budget: money,
-// 	expenses: {},
-// 	optionalExpenses: {},
-// 	income: [],
-// 	timeData: time,
-// 	savings: false
-// };
+let money = +prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-// let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-// 	a2 = prompt("Во сколько обойдется?", ''),
-// 	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-// 	a4 = prompt("Во сколько обойдется?", '');
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
+};
 
-// appData.expenses.a1 = a2;
-// appData.expenses.a3 = a4;
-
-// alert(appData.budget / 30);
-
-let num = 50;
-
-switch (num) {
-	case num < 49:
-		console.log('num < 49');
-		break;
-	case num == 49:
-		console.log("num equal 49")
-		break;
-	default:
-		console.log("num equal 50")
-		break;
-
+for (let i = 0; i < 2; i++){
+	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+		b = prompt("Во сколько обойдется?", '');
+	if ((typeof (a)) === "string" && (typeof (a)) != null
+	&& (typeof(b)) != null && a != '' && b != '' && a.length < 50
+	) {
+		console.log("done");
+		appData.expenses[a] = b;
+	}
+	else {
+		let a = prompt("New Введите обязательную статью расходов в этом месяце", ''),
+		b = prompt("New Во сколько обойдется?", '');
+	}
+	
 }
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Ежедневный бюджет:" + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+	console.log("Минимальный уровень достатка");
+}
+else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 1000){
+	console.log("Средний уровень достатка");
+}
+else if (appData.moneyPerDay > 1000){
+	console.log("Высокий уровень достатка");
+}
+else {
+	console.log("Произошла ошибка");
+}
+
